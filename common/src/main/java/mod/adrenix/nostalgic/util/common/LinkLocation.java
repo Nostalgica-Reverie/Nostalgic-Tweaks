5b8bf2a2e6b663efcf7821a166b18bcb1de16387
Adrenix <15348635+Adrenix@users.noreply.github.com>
2025-06-26 18:43:40 -0400

    Update discord invite link

--- common/src/main/java/mod/adrenix/nostalgic/util/common/LinkLocation.java
+++ common/src/main/java/mod/adrenix/nostalgic/util/common/LinkLocation.java
@@ -6,7 +6,7 @@ package mod.adrenix.nostalgic.util.common;
 public interface LinkLocation
 {
     String GITHUB = "https://github.com/Adrenix/Nostalgic-Tweaks/";
-    String DISCORD = "https://discord.gg/jWdfVh3";
+    String DISCORD = "https://discord.gg/Un7b9AWSsu";
     String KOFI = "https://ko-fi.com/adrenix";
     String GOLDEN_DAYS = "https://github.com/PoeticRainbow/golden-days/releases";
     String DOWNLOAD = "https://modrinth.com/mod/nostalgic-tweaks/versions";
