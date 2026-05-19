e5f85793bea11be21a7909d948d37e22f2abba92
Adrenix <15348635+adrenix@users.noreply.github.com>
2026-05-19 19:47:54 -0400

    fix(tweak): #150 fixed momentum bug when bed bounce is disabled

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/mechanics_block/BedBlockMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/mechanics_block/BedBlockMixin.java
@@ -1,9 +1,7 @@
 package mod.adrenix.nostalgic.mixin.tweak.gameplay.mechanics_block;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import mod.adrenix.nostalgic.tweak.config.GameplayTweak;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.BedBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,29 +13,14 @@ public abstract class BedBlockMixin
      * Disables the reduction in fall damage when an entity falls on a bed block.
      */
     @ModifyExpressionValue(
-        method = "fallOn",
-        at = @At(
-            value = "CONSTANT",
-            args = "floatValue=0.5F"
-        )
-    )
-    private float nt_mechanics_block$modifyFallDistanceMultiplier(float multiplier)
-    {
-        return GameplayTweak.DISABLE_BED_BOUNCE.get() ? 1.0F : multiplier;
-    }
-
-    /**
-     * Disables the ability for entities to bounce on bed blocks.
-     */
-    @WrapWithCondition(
-        method = "bounceUp",
+        method = "updateEntityAfterFallOn",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/Entity;setDeltaMovement(DDD)V"
+            target = "Lnet/minecraft/world/entity/Entity;isSuppressingBounce()Z"
         )
     )
-    private boolean nt_mechanics_block$shouldEntityBounce(Entity entity, double x, double y, double z)
+    private boolean nt_mechanics_block$modifySuppressingBounceFlag(boolean isSuppressingBounce)
     {
-        return !GameplayTweak.DISABLE_BED_BOUNCE.get();
+        return GameplayTweak.DISABLE_BED_BOUNCE.get() || isSuppressingBounce;
     }
 }
